e5f1f8067b214f90caea4ffe9fd50990e41159fe
Shinichiro Yamamoto <shinichiro.yam@gmail.com>
2015-12-24 15:00:15 +0900

    Initial Commit

---
+++ apiTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apiTest
+//
+//  Created by Shinichiro Yamamoto on 2015/12/24.
+//  Copyright © 2015年 shinichiro yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
